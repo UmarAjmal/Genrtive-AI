22ed30786f2d7b7295a37c5405f253e42e6e73be
Muhammad Umar Ajmal <126502013+UmarAjmal@users.noreply.github.com>
2026-02-08 21:18:16 +0500

    New Learings

--- Prompts/Summerizer/dynamic_prompt_sumerizer.py
+++ Prompts/Summerizer/dynamic_prompt_sumerizer.py
@@ -1,7 +1,7 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 import streamlit as st
 
 load_dotenv()
@@ -14,20 +14,8 @@ paper_input = st.selectbox( "Select Research Paper Name", ["Select...", "Attenti
 style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] )
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-template = PromptTemplate(
-    template = """"Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}
-Explanation Length: {length_input}
-1. Mathematical Details:
--Include relevant mathematical equations if present in the paper.
--Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-2. Analogies:
--Use relatable analogies to simplify complex ideas.
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
-Ensure the summary is clear, accurate, and aligned with the provided style and length.""",
-input_variables=["paper_input", "style_input", "length_input"]
-)
-
+template = load_prompt("dynamic_prompt_sumerizer_template.json")
+   
 prompt = template.invoke({
     "paper_input": paper_input,
     "style_input": style_input,
